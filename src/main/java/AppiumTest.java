a6732654fbc44a757a6401b301e07411fa762cd2
saiashish9 <saiashish3760@gmail.com>
2022-03-15 14:25:28 +0530

    completed

--- src/main/java/AppiumTest.java
+++ src/main/java/AppiumTest.java
@@ -11,9 +11,13 @@ public class AppiumTest {
     public static void main(String[] args) throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "iOS");
+//        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.2");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.3.1");
         desiredCapabilities.setCapability("udid", "00008101-000164922669001E");
+//        desiredCapabilities.setCapability("udid", "EBDA7F9A-95AF-4A37-A04E-2FCF311E2AEC");
+
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Sai's iPhone");
+//        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 13");
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
 //        desiredCapabilities.setCapability("bundleId"," com.saiashish.WebDriverAgentRunner.xctrunner");
         //desiredCapabilities.setCapability("noReset", "true");
@@ -21,7 +25,10 @@ public class AppiumTest {
 //        desiredCapabilities.setCapability("appActivity", ".ApiDemos");
 //        desiredCapabilities.setCapability("avd", "Pixel_2");
 //        IF APP IS ALREADY INSTALLED < COMMENT BELOW LINE
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/saiashishdarapureddy/Desktop/folders/IOS Development/XCUIAutomationScripts/XCUIAutomationScripts.ipa");
+//        desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/saiashishdarapureddy/Desktop/folders/IOS Development/XCUIAutomationScripts/XCUIAutomationScripts.ipa");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/saiashishdarapureddy/Desktop/folders/IOS Development/XCUIAutomationScripts/XCUIAutomationScriptsUITest.zip");
+//        desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/saiashishdarapureddy/Desktop/folders/IOS Development/XCUIAutomationScripts/build/Release-iphonesimulator/XCUIAutomationScripts.zip"); // .app
+//        desiredCapabilities.setCapability(MobileCapabilityType.APP,"/Users/saiashishdarapureddy/Library/Developer/Xcode/DerivedData/XCUIAutomationScripts-aqsolsfgsvvjtgefwibjhgovxwir/Build/Products/Debug-iphoneos/XCUIAutomationScriptsUITests-Runner.app");
 //      copy and paste the .ipa compress it, right click archive utility, show package identifier => bundle identifier
 //
         //        .zip => simulator
